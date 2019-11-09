43a21ccd1cde5e0620af11be872af8892a0ea76b
Billuc <billuc@hotmail.fr>
2019-11-09 19:55:12 +0100

    Ajout de spotdl et Début de code des classes

--- src/main/java/Fenetre.java
+++ src/main/java/Fenetre.java
@@ -1,2 +1,17 @@
-public class Fenetre {
+import javax.swing.*;
+
+public class Fenetre extends JFrame {
+    Vue laVue;
+
+    public Fenetre(int pWidth, int pHeight) {
+        super("Music Downloader");
+        this.setSize(pWidth, pHeight);
+        this.setLocationByPlatform(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        laVue = new Vue();
+        this.add(laVue);
+
+        this.setVisible(true);
+    }
 }
--- src/main/java/Vue.java
+++ src/main/java/Vue.java
@@ -1,2 +1,33 @@
-public class Vue {
+import javax.swing.*;
+
+public class Vue extends JPanel {
+    JLabel artistLabel;
+    JTextArea artistField;
+
+    JLabel songLabel;
+    JTextArea songField;
+
+    JLabel playlistURLLabel;
+    JTextArea playlistURLField;
+
+    JLabel albumURLLabel;
+    JTextArea albumURLField;
+
+    JCheckBox withMetaDataCheckBox;
+    boolean withMetaData;
+
+    JLabel songFolderLabel;
+    JTextArea songFolderField;
+    JButton folderChooserButton;
+
+    JComboBox extensionChooser;
+
+    JCheckBox trimMusicCheckBox;
+    boolean trimMusic;
+
+    public Vue() {
+        super();
+
+
+    }
 }
